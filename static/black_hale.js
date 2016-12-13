e6ccd063d03a5b27f60480edd52a658b0b32f28c
TankaMelnyk <tankamelnik@gmail.com>
2016-12-13 21:57:07 +0200

    Upload static objects

---
+++ static/black_hale.js
@@ -0,0 +1,49 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+function BlackHale(visualContainer, collisionManager, positionMapper, scoreManager) {
+    this.baseBlackHale = SpaceObjects;
+    this.baseBlackHale(visualContainer, collisionManager, positionMapper, scoreManager);
+
+    this.visualElement_.style.width = 70 + "px";
+    this.visualElement_.style.height = 70 + "px";
+    this.visualElement_.style.backgroundImage = "url(pictures/blackHale/blackHale1.png)";
+ 
+    this.counterLife_ = 0;
+    this.pointsForSpace_ = 150;
+    this.countImg_ = 1;
+}
+//---------------------------------------------------------------------------------------------------------------------
+BlackHale.prototype = Object.create(SpaceObjects.prototype);
+//---------------------------------------------------------------------------------------------------------------------
+BlackHale.prototype.constructor = BlackHale;
+//---------------------------------------------------------------------------------------------------------------------
+BlackHale.prototype.registerObjectToUI = function(positionPoint) {
+    SpaceObjects.prototype.registerObjectToUI.call(this, positionPoint);
+    this.scoreManager_.increaseBlackHaleCounter();
+};
+//---------------------------------------------------------------------------------------------------------------------
+BlackHale.prototype.doAction = function() {
+    if (this.countImg_ === 5) {
+        this.countImg_ = 1;
+        }
+    this.visualElement_.style.backgroundImage = "url(pictures/blackHale/blackHale" + this.countImg_ + ".png)";
+        this.countImg_++;
+};
+//---------------------------------------------------------------------------------------------------------------------
+BlackHale.prototype.destroyByObjectCollision = function() {}    // переопредиление, не сталкивающийся
+//---------------------------------------------------------------------------------------------------------------------
+BlackHale.prototype.destroyByBorderCollision = function() {
+    if (this.isObjectAlive_) {
+        SpaceObjects.prototype.destroyByObjectCollision.call(this);
+
+        this.scoreManager_.decreaseBlackHaleCounter();
+    }
+}
+//---------------------------------------------------------------------------------------------------------------------
+BlackHale.prototype.destroyByClick = function() {
+    if (this.isObjectAlive_) {
+        SpaceObjects.prototype.destroyByObjectCollision.call(this);
+        this.scoreManager_.establishThroughGames(this.pointsForSpace_);
+        this.scoreManager_.decreaseBlackHaleCounter();
+    }
+}
+//---------------------------------------------------------------------------------------------------------------------
---
+++ static/habitable_planet.js
@@ -0,0 +1,55 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+function HabitablePlanet(visualContainer, collisionManager, positionMapper, scoreManager) {
+    this.baseHabitablePlanet = SpaceObjects;
+    this.baseHabitablePlanet(visualContainer, collisionManager, positionMapper, scoreManager);
+
+    this.visualElement_.style.width = 100 + "px";
+    this.visualElement_.style.height = 100 + "px";
+    this.visualElement_.style.backgroundImage = "url(pictures/habib.png)";
+
+    this.pointsForSpace_ = -80;
+}
+//---------------------------------------------------------------------------------------------------------------------
+HabitablePlanet.prototype = Object.create(SpaceObjects.prototype);
+//---------------------------------------------------------------------------------------------------------------------
+HabitablePlanet.prototype.constructor = HabitablePlanet;
+//---------------------------------------------------------------------------------------------------------------------
+
+HabitablePlanet.prototype.registerObjectToUI = function(positionPoint) {
+    SpaceObjects.prototype.registerObjectToUI.call(this, positionPoint);
+
+    this.scoreManager_.increaseHabitableAndSpaceCounter();                        /// счетчик планет
+    this.scoreManager_.increaseHabitableCounter();
+};
+//---------------------------------------------------------------------------------------------------------------------
+HabitablePlanet.prototype.doAction = function() {
+    // обнулили действие объекта
+};
+//---------------------------------------------------------------------------------------------------------------------
+HabitablePlanet.prototype.destroyByObjectCollision = function() {
+    if (this.isObjectAlive_) {
+        SpaceObjects.prototype.destroyByObjectCollision.call(this);
+
+        this.scoreManager_.decreaseHabitableAndSpaceCounter();
+        this.scoreManager_.decreaseHabitableCounter();
+        this.scoreManager_.establishThroughGames(this.pointsForSpace_);
+    }
+}
+//---------------------------------------------------------------------------------------------------------------------
+HabitablePlanet.prototype.destroyByClick = function() {
+    if (this.isObjectAlive_) {
+        SpaceObjects.prototype.destroyByClick.call(this);
+
+        this.scoreManager_.decreaseHabitableAndSpaceCounter();
+        this.scoreManager_.decreaseHabitableCounter();
+    }
+}
+//---------------------------------------------------------------------------------------------------------------------
+HabitablePlanet.prototype.destroyByBorderCollision = function() {
+    if (this.isObjectAlive_) {
+        SpaceObjects.prototype.destroyByBorderCollision.call(this);
+        this.scoreManager_.decreaseHabitableCounter();
+        this.scoreManager_.decreaseHabitableAndSpaceCounter();
+    }
+}
+//---------------------------------------------------------------------------------------------------------------------
